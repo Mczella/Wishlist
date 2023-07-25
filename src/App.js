ee33cc4bf8be7f31f5a3aaad8b4d40206053f09d
Mczella <markaczova@gmail.com>
2023-07-25 19:40:43 +0200

    App.js react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, {useContext} from 'react'
 import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom"
-import './styles/App.css';
-import Home from "./Home";
+import './styles/App.css'
+import Login from "./Login"
+import Home from "./Home"
+import {AuthorizationContext} from "./AuthorizationContext"
+import Navbar from "./Navbar"
+import Users from "./Users"
+import WelcomePage from "./WelcomePage";
+import RegisterUser from "./RegisterUser";
 
 const App = () => {
 
+    const {currentUser} = useContext(AuthorizationContext)
+    const RequireAuth = ({ children }) => {
+        return currentUser ? children : <Navigate to="/login" />
+    }
 
     return (
         <Router>
             <div className="App">
+
                 <Routes>
+                        <Route path="/" element={<WelcomePage/>} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<RegisterUser />} />
                         <Route
-                            path="/"
+                            path="/home"
                             element={
-                                    <Home />
+                                <RequireAuth>
+                                    <Navbar/>
+                                    <Home/>
+                                </RequireAuth>
+                            }
+                        />
+                        <Route
+                            path="/users"
+                            element={
+                                <RequireAuth>
+                                    <Navbar/>
+                                    <Users />
+                                </RequireAuth>
                             }
                         />
                 </Routes>
             </div>
         </Router>
-    );
-};
+    )
+}
 
-export default App;
+export default App
