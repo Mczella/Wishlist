29bd396e343e5238732054e5155d4b68b3c1c282
Mczella <markaczova@gmail.com>
2023-07-25 19:42:54 +0200

    SignOut implementation

---
+++ src/SignOut.jsx
@@ -0,0 +1,31 @@
+import { signOut } from "firebase/auth"
+import {useNavigate} from "react-router-dom"
+import {AuthorizationContext} from "./AuthorizationContext"
+import {useContext, useState} from "react"
+import { auth } from "./firebase"
+
+const SignOut = () => {
+
+    const navigate = useNavigate()
+    const {dispatch} = useContext(AuthorizationContext)
+    const [error, setError] = useState("")
+
+
+    const handleSignOut = async () => {
+        try {
+            await signOut(auth)
+            dispatch({ type: "LOGOUT" })
+            navigate("/login")
+        } catch (error) {
+            setError(true)
+            console.log(error)
+        }
+    };
+
+    return (
+        // add popup with error
+        <a href="#" onClick={handleSignOut}>Odhlásit</a>
+    )
+}
+
+export default SignOut
