57f80fb5b8b97311fe981fb8abd17473b340bec8
Mczella <markaczova@gmail.com>
2023-07-27 21:33:59 +0200

    Add creator

---
+++ src/AddNewUser.jsx
@@ -0,0 +1,121 @@
+import {useForm} from "react-hook-form"
+import {handleAdd} from "./Crud"
+import React, {useState} from "react"
+import {
+    Box,
+    Button,
+    Flex,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Heading,
+    Input,
+    Stack,
+    Text
+} from "@chakra-ui/react";
+
+
+const AddNewUser = ({onSimpleClose}) => {
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm({mode: "onBlur"})
+    const [error, setError] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onSubmit = async (data) => {
+        setIsLoading(true)
+        //if user does not exist already
+        try {
+            const updatedData = {...data, admin: false}
+            handleAdd(updatedData, "users")
+        } catch (error) {
+            setError(true)
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+            if (!error) {
+                onSimpleClose()
+            }
+        }
+    }
+
+
+    return (
+        <Flex
+            minH={0}
+            align={'baseline'}
+            justify={'center'}
+            bg={'gray.200'}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'} textAlign={'center'}>
+                        Vytvořte uživatele
+                    </Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        bez přihlašovacích údajů
+                    </Text>
+                </Stack>
+                <Box
+                    rounded={'lg'}
+                    bg={'white'}
+                    boxShadow={'lg'}
+                    p={8}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Stack spacing={4}>
+
+                            <FormControl id="name" isInvalid={errors.name}>
+                                <FormLabel>Jméno</FormLabel>
+                                <Input
+                                    name="name"
+                                    type="name"
+                                    placeholder="jméno"
+                                    className={`form-control ${errors.name && "invalid"}`}
+                                    {...register("name", {required: true})}
+                                />
+                                <FormErrorMessage>
+                                    {errors.name && 'Zadejte jméno.'}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                            <FormControl id="surname" isInvalid={errors.password}>
+                                <FormLabel>Příjmení</FormLabel>
+
+                                <Input
+                                    name="surname"
+                                    type="surname"
+                                    placeholder="příjmení"
+                                    {...register("surname", {required: true})}
+                                />
+                                <FormErrorMessage>
+                                    {errors.surname && 'Zadejte příjmení.'}
+                                </FormErrorMessage>
+                            </FormControl>
+                            <Stack spacing={6}>
+                                <Stack spacing={2} pt={2}>
+                                    <Button
+                                        isLoading={isLoading}
+                                        rounded={'full'}
+                                        colorScheme={'orange'}
+                                        bg={'orange.400'}
+                                        _hover={{bg: 'orange.500'}}
+                                        type="submit">
+                                        Vytvořit uživatele
+                                    </Button>
+                                    {error &&
+                                        <Text fontSize='xs' color='red'>
+                                            Při vytváření uživatelského účtu došlo k chybě. Zkuste to prosím znovu.
+                                        </Text>}
+
+                                </Stack>
+                            </Stack>
+                        </Stack>
+                    </form>
+                </Box>
+            </Stack>
+        </Flex>
+    )
+}
+
+export default AddNewUser
