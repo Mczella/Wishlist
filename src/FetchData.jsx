320f18be9a1b72befdac31913cd6625a4abdb529
Mczella <markaczova@gmail.com>
2023-08-05 20:20:59 +0200

    Add floating button and create portal

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -1,16 +1,20 @@
-import React, {useRef, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {getFunctions, httpsCallable} from "firebase/functions"
 import AddNewGift from "./AddNewGift"
 import {
-    Button, FormControl, FormErrorMessage, FormLabel, Input,
+    Box,
+    Button, Center, FormControl, FormErrorMessage, FormLabel, Heading, IconButton, Input,
     Modal,
     ModalBody,
     ModalCloseButton,
     ModalContent, ModalFooter,
     ModalHeader,
-    ModalOverlay,
+    ModalOverlay, Stack, Text,
     useDisclosure
 } from "@chakra-ui/react";
+import {AddIcon, EmailIcon} from "@chakra-ui/icons";
+import { useInView } from 'react-intersection-observer'
+import {createPortal} from "react-dom";
 
 const FetchData = () => {
     const [defaultValues, setDefaultValues] = useState({})
@@ -20,6 +24,9 @@ const FetchData = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const btnRef = React.useRef(null)
     const [isLoading, setIsLoading] = useState(false)
+    const { ref, inView } = useInView({
+        /* Optional options */
+        initialInView: true})
 
     const fetchMetaData = async (e) => {
         e.preventDefault()
@@ -50,11 +57,52 @@ const FetchData = () => {
         setIsLoading(false)
     };
 
+  useEffect(() => {
+        if (!inView) {
+            console.log('Element is not in view!');
+        } else if (inView) {
+            console.log("in view")
+        }
+    }, [inView]);
+
     return (
         <div>
-            <Button mt={3} ref={btnRef} onClick={onOpen}>
+            <Button
+                width={'100%'}
+                ref={ref}
+                Button leftIcon={<AddIcon />}
+                colorScheme='orange'
+                rounded={'lg'}
+                bg={'orange.400'}
+                _hover={{bg: 'orange.500'}}
+                variant='solid'
+                onClick={onOpen}>
                 Přidat dárek
             </Button>
+
+            {createPortal(
+                <IconButton
+                    display={!inView? 'inherit' : 'none'}
+                    position={'fixed'}
+                    bottom={'5%'}
+                    right={'5%'}
+                    isRound={true}
+                    variant='solid'
+                    colorScheme='orange'
+                    aria-label='Add'
+                    size={'lg'}
+                    fontSize='20px'
+                    boxShadow={'lg'}
+                    _hover={{bg: 'orange.400'}}
+
+
+                    onClick={onOpen}
+                    icon={<Center><AddIcon/></Center>}
+                />,
+                document.body
+            )}
+
+
             <Modal
                 onClose={onClose}
                 finalFocusRef={btnRef}
@@ -62,12 +110,23 @@ const FetchData = () => {
                 scrollBehavior="inside"
             >
                 <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader><h2>Přidejte nový dárek</h2></ModalHeader>
+                <ModalContent bg={'gray.200'}>
+                    <ModalHeader>
+                        <Heading fontSize={'4xl'} textAlign={'center'} mt={8} mb={4}>
+                            Přidejte nový dárek
+                        </Heading>
+                    </ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
+                        <Box
+                            align={'baseline'}
+                            justify={'center'}
+                            rounded={'lg'}
+                            bg={'white'}
+                            boxShadow={'lg'}
+                            p={8}>
                         <form onSubmit={fetchMetaData}>
-                            <FormControl id="link" >
+                            <FormControl id="link">
                             <FormLabel htmlFor="metadataLink">Pro rychlejší vyplnění formuláře vložte odkaz na dárek.</FormLabel>
                             <Input
                                 name="metadataLink"
@@ -78,16 +137,27 @@ const FetchData = () => {
                                 onChange={(e) => setLink(e.target.value)}
                             />
 
-                            <Button type="submit" isLoading={isLoading}>Vložit</Button>
-                                <FormErrorMessage>
-                                    Bohužel se nepodařilo načíst data, zkuste to později nebo je zadejte ručně.
-                                </FormErrorMessage>
+                                <Stack spacing={6}>
+                                    <Stack spacing={2} pt={2}>
+                                        <Button isLoading={isLoading}
+                                                rounded={'lg'}
+                                                colorScheme={'orange'}
+                                                bg={'orange.400'}
+                                                _hover={{bg: 'orange.500'}}
+                                                mb={4}
+                                                type="submit">
+                                            Vložit
+                                        </Button>
+                                        {error &&
+                                            <Text fontSize='xs' color='red'>Bohužel se nepodařilo načíst data, zkuste to později nebo je zadejte ručně.</Text>}
+                                    </Stack>
+                                </Stack>
                             </FormControl>
                         </form>
-                        <AddNewGift defaultValues={defaultValues}/>
+                        <AddNewGift defaultValues={defaultValues} onClose={onClose}/>
+                        </Box>
                     </ModalBody>
                     <ModalFooter>
-                        <Button onClick={onClose}>Close</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
