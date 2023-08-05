8930d26dfdfd1246d67458fd402fe4a3374dc0a0
Mczella <markaczova@gmail.com>
2023-08-05 20:29:33 +0200

    Add style

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,15 @@
-import GiftStorage from "./GiftStorage";
-import FetchData from "./FetchData";
+import Gifts from "./Gifts"
+import FetchData from "./FetchData"
+import {Stack} from "@chakra-ui/react";
 
 const Home = () => {
 
 
         return (
-            <>
+            <Stack px={[4, 8, 12, 16]} py={8}>
             <FetchData/>
-            <GiftStorage/>
-            </>
+            <Gifts/>
+            </Stack>
         )
 }
 
