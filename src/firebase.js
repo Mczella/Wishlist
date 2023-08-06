b2d96920f1f26f151c0f544ec2a355fa0861a939
Mczella <markaczova@gmail.com>
2023-08-06 23:09:24 +0200

    Clean up

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,10 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getFunctions } from 'firebase/functions';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: "mcz-christmas.firebaseapp.com",
@@ -18,14 +13,10 @@ const firebaseConfig = {
     messagingSenderId: "734981717904",
     appId: "1:734981717904:web:c1f8f8d2e6b4a152db3d15",
     measurementId: "G-MWFXRT59NJ"
-};
+}
 
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getFirestore(app);
-export const auth = getAuth();
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+export const db = getFirestore(app)
+export const auth = getAuth()
 export const storage = getStorage(app)
-
-export const functions = getFunctions();
